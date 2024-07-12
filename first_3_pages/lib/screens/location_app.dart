1365665446669802bff42f5838c233bd4b016638
Ajitha-132006 <167690242+Ajitha-132006@users.noreply.github.com>
2024-07-12 19:33:37 +0530

    Update location_app.dart

--- first_3_pages/lib/screens/location_app.dart
+++ first_3_pages/lib/screens/location_app.dart
@@ -33,25 +33,15 @@ class _HomeScreenState extends State<HomeScreen> {
   bool gpsEnabled = false;
   bool permissionGranted = false;
   l.Location location = l.Location();
-  late StreamSubscription subscription;
+  late StreamSubscription<l.LocationData> subscription;
   bool trackingEnabled = false;
 
-  LatLng? currentLocation;
-  GoogleMapController? mapController;
-  CameraPosition? initialCameraPosition;
+  late GoogleMapController mapController;
+  Marker? userMarker;
 
   @override
   void initState() {
     super.initState();
-    // Ensure MediaQuery is available before accessing size
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      setState(() {
-        initialCameraPosition = const CameraPosition(
-          target: LatLng(0.0, 0.0), // Set your default initial position here
-          zoom: 15.0,
-        );
-      });
-    });
     checkStatus();
   }
 
@@ -68,76 +58,53 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text('Location App'),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            buildListTile(
-              "GPS",
-              gpsEnabled
-                  ? const Text("Okey")
-                  : ElevatedButton(
-                      onPressed: () {
-                        requestEnableGps();
-                      },
-                      child: const Text("Enable Gps")),
-            ),
-            buildListTile(
-              "Permission",
-              permissionGranted
-                  ? const Text("Okey")
-                  : ElevatedButton(
-                      onPressed: () {
-                        requestLocationPermission();
-                      },
-                      child: const Text("Request Permission")),
-            ),
-            buildListTile(
-              "Location",
-              trackingEnabled
-                  ? ElevatedButton(
-                      onPressed: () {
-                        stopTracking();
-                      },
-                      child: const Text("Stop"))
-                  : ElevatedButton(
-                      onPressed: gpsEnabled && permissionGranted
-                          ? () {
-                              startTracking();
-                            }
-                          : null,
-                      child: const Text("Start")),
-            ),
-            Expanded(
-              child: GoogleMap(
-                onMapCreated: _onMapCreated,
-                initialCameraPosition: initialCameraPosition ??
-                    const CameraPosition(
-                      target: LatLng(0.0,
-                          0.0), // Fallback position if initialCameraPosition is null
-                      zoom: 15.0,
-                    ),
-                markers: Set.of(_createMarkers()),
-                myLocationEnabled: true,
-                myLocationButtonEnabled: true,
-                mapType: MapType.normal,
-                onTap: (LatLng latLng) {
-                  // Handle map tap if needed
-                },
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Expanded(
+            child: GoogleMap(
+              initialCameraPosition: const CameraPosition(
+                target: LatLng(0, 0),
+                zoom: 15.0,
               ),
+              markers: userMarker != null ? {userMarker!} : {},
+              onMapCreated: (controller) {
+                mapController = controller;
+              },
             ),
-          ],
-        ),
+          ),
+          buildListTile(
+            "GPS",
+            gpsEnabled
+                ? const Text("Okey")
+                : ElevatedButton(
+                    onPressed: requestEnableGps,
+                    child: const Text("Enable Gps")),
+          ),
+          buildListTile(
+            "Permission",
+            permissionGranted
+                ? const Text("Okey")
+                : ElevatedButton(
+                    onPressed: requestLocationPermission,
+                    child: const Text("Request Permission")),
+          ),
+          buildListTile(
+            "Location",
+            trackingEnabled
+                ? ElevatedButton(
+                    onPressed: stopTracking, child: const Text("Stop"))
+                : ElevatedButton(
+                    onPressed:
+                        gpsEnabled && permissionGranted ? startTracking : null,
+                    child: const Text("Start")),
+          ),
+        ],
       ),
     );
   }
 
-  ListTile buildListTile(
-    String title,
-    Widget? trailing,
-  ) {
+  ListTile buildListTile(String title, Widget? trailing) {
     return ListTile(
       dense: true,
       title: Text(title),
@@ -150,32 +117,18 @@ class _HomeScreenState extends State<HomeScreen> {
       log("Already open");
     } else {
       bool isGpsActive = await location.requestService();
-      if (!isGpsActive) {
-        setState(() {
-          gpsEnabled = false;
-        });
-        log("User did not turn on GPS");
-      } else {
-        log("Gave permission to the user and opened it");
-        setState(() {
-          gpsEnabled = true;
-        });
-      }
+      setState(() {
+        gpsEnabled = isGpsActive;
+      });
     }
   }
 
   void requestLocationPermission() async {
     PermissionStatus permissionStatus =
         await Permission.locationWhenInUse.request();
-    if (permissionStatus == PermissionStatus.granted) {
-      setState(() {
-        permissionGranted = true;
-      });
-    } else {
-      setState(() {
-        permissionGranted = false;
-      });
-    }
+    setState(() {
+      permissionGranted = permissionStatus == PermissionStatus.granted;
+    });
   }
 
   Future<bool> isPermissionGranted() async {
@@ -186,54 +139,12 @@ class _HomeScreenState extends State<HomeScreen> {
     return await Permission.location.serviceStatus.isEnabled;
   }
 
-  void checkStatus() async {
-    bool permission = await isPermissionGranted();
-    bool gps = await isGpsEnabled();
+  checkStatus() async {
+    bool permissionGranted = await isPermissionGranted();
+    bool gpsEnabled = await isGpsEnabled();
     setState(() {
-      permissionGranted = permission;
-      gpsEnabled = gps;
-    });
-  }
-
-  Set<Marker> _createMarkers() {
-    if (currentLocation != null) {
-      return {
-        Marker(
-          markerId: const MarkerId("currentLocation"),
-          position: currentLocation!,
-          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
-        ),
-      };
-    } else {
-      return {};
-    }
-  }
-
-  void _onMapCreated(GoogleMapController controller) {
-    setState(() {
-      mapController = controller;
-    });
-  }
-
-  void addLocation(l.LocationData data) {
-    setState(() {
-      currentLocation = LatLng(data.latitude!, data.longitude!);
-    });
-    if (mapController != null) {
-      mapController!.animateCamera(
-        CameraUpdate.newCameraPosition(
-          CameraPosition(
-            target: currentLocation!,
-            zoom: 15.0,
-          ),
-        ),
-      );
-    }
-  }
-
-  void clearLocation() {
-    setState(() {
-      currentLocation = null;
+      this.permissionGranted = permissionGranted;
+      this.gpsEnabled = gpsEnabled;
     });
   }
 
@@ -244,8 +155,8 @@ class _HomeScreenState extends State<HomeScreen> {
     if (!(await isPermissionGranted())) {
       return;
     }
-    subscription = location.onLocationChanged.listen((l.LocationData event) {
-      addLocation(event);
+    subscription = location.onLocationChanged.listen((event) {
+      updateLocation(event);
     });
     setState(() {
       trackingEnabled = true;
@@ -257,6 +168,18 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {
       trackingEnabled = false;
     });
-    clearLocation();
+  }
+
+  void updateLocation(l.LocationData data) {
+    LatLng newPosition = LatLng(data.latitude!, data.longitude!);
+    setState(() {
+      userMarker = Marker(
+        markerId: const MarkerId('userMarker'),
+        position: newPosition,
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+      );
+    });
+    mapController.animateCamera(CameraUpdate.newLatLng(newPosition));
   }
 }
+
